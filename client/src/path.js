41ecbc87c81983cb24ab8361484d33d9745b8b5d
mtdanov <mtdanov@gmail.com>
2024-08-12 17:25:39 +0300

    add paths

---
+++ client/src/path.js
@@ -0,0 +1,24 @@
+
+const Path = {
+    Home: "/",
+    Register: "/register",
+    Login: "/login",
+    About: "/about",
+    Blog: "/blog",
+    Cart: "/cart",
+    NotFound: '/404',
+    Logout: '/logout',
+    DogFood: '/dog-food',
+    CatFood: '/cat-food',
+    BlogDetails: '/details/:articleId',
+    Service: '/services',
+    Profile: '/profile',
+    Contact: '/contact',
+    DogCategory: '/dog-category',
+    CatCategory: '/cat-category',
+    Adoption: '/adoption',
+    CreatePost: 'create-post',
+  };
+  
+  export default Path;
+  
