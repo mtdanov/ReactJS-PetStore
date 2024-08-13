ef20ae9e02955ce561b5ccb6f41fdcc5799c920b
mtdanov <mtdanov@gmail.com>
2024-08-13 19:51:50 +0300

    add admin guard

---
+++ client/src/guards/AdminGuard.jsx
@@ -0,0 +1,18 @@
+import { useContext } from "react"
+import { Navigate, Outlet } from "react-router-dom";
+import AuthContext from "../contexts/authContext"
+import Path from "../path";
+
+export default function AdminGuard() {
+    const { role, isAuthenticated } = useContext(AuthContext);
+
+    if (!isAuthenticated) {
+        return <Navigate to={Path.Login} />
+    }
+
+    if (role !== 'admin') {
+        return <Navigate to={Path.NotFound} />;
+    }
+
+    return <Outlet />;
+}
