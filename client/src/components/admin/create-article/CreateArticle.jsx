6f14864707e17b1c0d25c5ef57ad669e49fec67a
mtdanov <mtdanov@gmail.com>
2024-08-15 15:23:33 +0300

    add spinner

--- client/src/components/admin/create-article/CreateArticle.jsx
+++ client/src/components/admin/create-article/CreateArticle.jsx
@@ -1,9 +1,11 @@
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 import * as articeService from '../../../services/articleService'
 import { useValidation } from '../../../hooks/useValidation';
 import useForm from '../../../hooks/useForm';
 import Path from '../../../path';
+import Preloader from '../../Preloader/Preloader';
 
 import upload_pic from '../../../../public/images/upload.png'
 import styles from './CreateArticle.module.css'
@@ -16,12 +18,16 @@ const initialState = {
 export default function CreateArticle() {
     const navigate = useNavigate()
     const [validateForm, errors] = useValidation()
+    const [isloading, setloading] = useState(false)
+
 
     const { data, onChange, onSubmit, } = useForm(initialState, async (formData) => {
         try {
             if (validateForm('article', formData)) {
+                setloading(true)
                 await articeService.createArticle(formData)
                 navigate(Path.Blog)
+                setloading(false)
             }
         } catch (error) {
             console.log(error.message);
@@ -29,35 +35,39 @@ export default function CreateArticle() {
     });
 
     return (
-        <section className={styles.createArticle}>
-            <form className={styles.articleForm} onSubmit={onSubmit}>
-                <h2 className={styles.articleTitle}>Създай Статия</h2>
+        <>
+            {isloading ? <Preloader /> :
+                <section className={styles.createArticle}>
+                    <form className={styles.articleForm} onSubmit={onSubmit}>
+                        <h2 className={styles.articleTitle}>Създай Статия</h2>
 
-                <div className={styles.createAnimaltName}>
-                    <label className={styles.nameLabel} htmlFor="title">Заглавие:</label>
-                    <input className={styles.nameInput} name='title' value={data.name} onChange={onChange} type="text" />
-                    {errors.title && <span className={styles.error}>{errors.title}</span>}
-                </div>
+                        <div className={styles.createAnimaltName}>
+                            <label className={styles.nameLabel} htmlFor="title">Заглавие:</label>
+                            <input className={styles.nameInput} name='title' value={data.name} onChange={onChange} type="text" />
+                            {errors.title && <span className={styles.error}>{errors.title}</span>}
+                        </div>
 
-                <div className={styles.createAnimalDescription}>
-                    <label className={styles.descriptionLabel} htmlFor="description">Описание:</label>
-                    <textarea className={styles.description} name="description" value={data.description} onChange={onChange} id=""></textarea>
-                    {errors.description && <span className={styles.error}>{errors.description}</span>}
-                </div>
+                        <div className={styles.createAnimalDescription}>
+                            <label className={styles.descriptionLabel} htmlFor="description">Описание:</label>
+                            <textarea className={styles.description} name="description" value={data.description} onChange={onChange} id=""></textarea>
+                            {errors.description && <span className={styles.error}>{errors.description}</span>}
+                        </div>
 
-                <div className={styles.articlePic}>
-                    <label htmlFor="file">
-                        <img className={styles.pic}
-                            src={data.file ? URL.createObjectURL(data.file) : upload_pic}
-                            alt=""
-                        />
-                    </label>
-                    <input type="file" className={styles.file} name='file' placeholder='Качи Снимка' onChange={onChange} />
-                    {errors.file && <span className={styles.error}>{errors.file}</span>}
-                </div>
-
-                <button className={styles.createBtn}>Качи</button>
-            </form>
-        </section>
+                        <div className={styles.articlePic}>
+                            <label htmlFor="file">
+                                <img className={styles.pic}
+                                    src={data.file ? URL.createObjectURL(data.file) : upload_pic}
+                                    alt=""
+                                />
+                            </label>
+                            <input type="file" className={styles.file} name='file' placeholder='Качи Снимка' onChange={onChange} />
+                            {errors.file && <span className={styles.error}>{errors.file}</span>}
+                        </div>
+                        <button className={styles.createBtn}>Качи</button>
+                    </form>
+                </section>
+            }
+        </>
     )
 }
+
