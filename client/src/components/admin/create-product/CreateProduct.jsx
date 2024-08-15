9eed54b234de72e7b0adc93254e80ca77e8dd47e
mtdanov <mtdanov@gmail.com>
2024-08-15 15:25:16 +0300

    add edit artilcle

--- client/src/components/admin/create-product/CreateProduct.jsx
+++ client/src/components/admin/create-product/CreateProduct.jsx
@@ -10,148 +10,155 @@ import { useValidation } from '../../../hooks/useValidation';
 
 import upload_pic from '../../../../public/images/upload.png'
 import styles from "./CreateProduct.module.css"
+import Preloader from '../../Preloader/Preloader';
 
 const initialState = {
-    name: '',
-    description: '',
-    price: '',
-    category: '',
-    totalPrice: 0,
-    type: '',
-    file: null,
+  name: '',
+  description: '',
+  price: '',
+  category: '',
+  totalPrice: 0,
+  type: '',
+  file: null,
 }
 
 
 export default function CreateProduct() {
-    const [validateForm, errors] = useValidation()
-    const { handleAddProductState } = useContext(ProductsContext)
-    const [categories, setCategories] = useState([]);
-    const navigate = useNavigate()
+  const [validateForm, errors] = useValidation()
+  const { handleAddProductState } = useContext(ProductsContext)
+  const [categories, setCategories] = useState([]);
+  const [isloading, setloading] = useState(false)
+  const navigate = useNavigate()
 
-    const { data, onChange, onSubmit } = useForm(initialState, async (formData) => {
-        try {
-            if (validateForm('product', formData)) {
-                const result = await productService.createProduct(formData)
-                console.log(result);
-                handleAddProductState(result)
-                navigate(Path.Home)
-            }
-        } catch (error) {
-            console.log(error.message);
-        }
-    });
+  const { data, onChange, onSubmit } = useForm(initialState, async (formData) => {
+    try {
+      if (validateForm('product', formData)) {
+        setloading(true)
+        const result = await productService.createProduct(formData)
+        handleAddProductState(result)
+        navigate(Path.Home)
+        setloading(false)
 
-    useEffect(() => {
-        categoryService.getCategories().then(result => setCategories(result)).catch(error => console.log(error))
-    }, [])
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  });
 
-    return (
+  useEffect(() => {
+    categoryService.getCategories().then(result => setCategories(result)).catch(error => console.log(error))
+  }, [])
 
-        <section className={styles.createProductPage}>
-            <div className={styles.postContainer}>
-                <form className={styles.postForm} onSubmit={onSubmit}>
-                    <h2 className={styles.postHeader}>Създай Продукт</h2>
+  return (
+    <> {isloading ? <Preloader /> :
+      <section className={styles.createProductPage}>
+        <div className={styles.postContainer}>
+          <form className={styles.postForm} onSubmit={onSubmit}>
+            <h2 className={styles.postHeader}>Създай Продукт</h2>
 
-                    <div className={styles.formGroup}>
-                        <label htmlFor="name">Име на продукта:</label>
-                        <input
-                            id="name"
-                            className={styles.formControl}
-                            value={data.name}
-                            name="name"
-                            type="text"
-                            placeholder="Име на продукта"
-                            onChange={onChange}
-                        />
-                        {errors.name && <span className={styles.error}>{errors.name}</span>}
-                    </div>
-
-                    <div className={styles.formGroup}>
-                        <label htmlFor="price">Цена:</label>
-                        <input
-                            id="price"
-                            className={styles.formControl}
-                            value={data.price}
-                            name="price"
-                            type="number"
-                            placeholder="Цена"
-                            onChange={onChange}
-                        />
-                        {errors.price && <span className={styles.error}>{errors.price}</span>}
-                    </div>
-
-                    <div className={styles.formGroup}>
-                        <label htmlFor="category">Категория:</label>
-                        <select
-                            id="category"
-                            className={styles.formControl}
-                            onChange={onChange}
-                            name="category"
-                            value={data.category}
-                        >
-                            <option value="">Избери</option>
-                            {categories.map((category) => (
-                                <option key={category.id} value={category.id}>
-                                    {category.name}
-                                </option>
-                            ))}
-                        </select>
-                        {errors.category && <span className={styles.error}>{errors.category}</span>}
-                    </div>
-
-                    <div className={styles.formGroup}>
-                        <label htmlFor="type">Вид:</label>
-                        <select
-                            id="type"
-                            className={styles.formControl}
-                            onChange={onChange}
-                            name="type"
-                            value={data.type}
-                        >
-                            <option value=""></option>
-                            <option value="medical">Лечебна</option>
-                            <option value="wet">Мокра</option>
-                            <option value="dry">Суха</option>
-                        </select>
-                        {errors.type && <span className={styles.error}>{errors.type}</span>}
-                    </div>
-
-                    <div className={styles.formGroup}>
-                        <label htmlFor="description">Описание:</label>
-                        <textarea
-                            id="description"
-                            className={styles.formControl}
-                            name="description"
-                            cols="30"
-                            rows="10"
-                            placeholder="Описание"
-                            onChange={onChange}
-                        />
-                        {errors.description && <span className={styles.error}>{errors.description}</span>}
-                    </div>
-
-                    <div className={styles.formGroup}>
-                        <label htmlFor="file">
-                            <img
-                                className={styles.pic}
-                                src={data.file ? URL.createObjectURL(data.file) : upload_pic}
-                                alt=""
-                            />
-                        </label>
-                        <input
-                            id="file"
-                            type="file"
-                            className={styles.formControl}
-                            name="file"
-                            placeholder="Качи Снимка"
-                            onChange={onChange}
-                        />
-                        {errors.file && <span className={styles.error}>{errors.file}</span>}
-                    </div>
-
-                    <button className={styles.createBtn}>Създай</button>
-                </form>
+            <div className={styles.formGroup}>
+              <label htmlFor="name">Име на продукта:</label>
+              <input
+                id="name"
+                className={styles.formControl}
+                value={data.name}
+                name="name"
+                type="text"
+                placeholder="Име на продукта"
+                onChange={onChange}
+              />
+              {errors.name && <span className={styles.error}>{errors.name}</span>}
             </div>
-        </section>
-    )
+
+            <div className={styles.formGroup}>
+              <label htmlFor="price">Цена:</label>
+              <input
+                id="price"
+                className={styles.formControl}
+                value={data.price}
+                name="price"
+                type="number"
+                min={0}
+                placeholder="Цена"
+                onChange={onChange}
+              />
+              {errors.price && <span className={styles.error}>{errors.price}</span>}
+            </div>
+
+            <div className={styles.formGroup}>
+              <label htmlFor="category">Категория:</label>
+              <select
+                id="category"
+                className={styles.formControl}
+                onChange={onChange}
+                name="category"
+                value={data.category}
+              >
+                <option value="">Избери</option>
+                {categories.map((category) => (
+                  <option key={category.id} value={category.id}>
+                    {category.name}
+                  </option>
+                ))}
+              </select>
+              {errors.category && <span className={styles.error}>{errors.category}</span>}
+            </div>
+
+            <div className={styles.formGroup}>
+              <label htmlFor="type">Вид:</label>
+              <select
+                id="type"
+                className={styles.formControl}
+                onChange={onChange}
+                name="type"
+                value={data.type}
+              >
+                <option value=""></option>
+                <option value="medical">Лечебна</option>
+                <option value="wet">Мокра</option>
+                <option value="dry">Суха</option>
+              </select>
+              {errors.type && <span className={styles.error}>{errors.type}</span>}
+            </div>
+
+            <div className={styles.formGroup}>
+              <label htmlFor="description">Описание:</label>
+              <textarea
+                id="description"
+                className={styles.formControl}
+                name="description"
+                cols="30"
+                rows="10"
+                placeholder="Описание"
+                onChange={onChange}
+              />
+              {errors.description && <span className={styles.error}>{errors.description}</span>}
+            </div>
+
+            <div className={styles.formGroup}>
+              <label htmlFor="file">
+                <img
+                  className={styles.pic}
+                  src={data.file ? URL.createObjectURL(data.file) : upload_pic}
+                  alt=""
+                />
+              </label>
+              <input
+                id="file"
+                type="file"
+                className={styles.formControl}
+                name="file"
+                placeholder="Качи Снимка"
+                onChange={onChange}
+              />
+              {errors.file && <span className={styles.error}>{errors.file}</span>}
+            </div>
+
+            <button className={styles.createBtn}>Създай</button>
+          </form>
+        </div>
+      </section>
+    }
+    </>
+  )
 }
---
+++ client/src/components/admin/edit-article/EditArticle.jsx
@@ -0,0 +1,113 @@
+import { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom';
+
+import useForm from '../../../hooks/useForm';
+import { useValidation } from '../../../hooks/useValidation';
+import * as articleService from '../../../services/articleService'
+import Path from '../../../path';
+import Preloader from '../../Preloader/Preloader';
+import useImageUpload from '../../../hooks/useImageUpload';
+
+import upload_pic from '../../../../public/images/upload.png'
+import styles from "./EditArticle.module.css"
+
+export default function EditArticle() {
+    const navigate = useNavigate()
+    const { id } = useParams()
+
+    const [isloading, setloading] = useState(false)
+    const [article, setArticle] = useState({
+        title: '',
+        description: '',
+        file: '',
+    })
+
+    const [validateForm, errors] = useValidation()
+
+    const { image, imageFile, onImageChange } = useImageUpload(article.file);
+
+    const { data, onChange, onSubmit, } = useForm(article, async (formData) => {
+        if (imageFile !== null) {
+            formData.append('file', imageFile);
+        } else if (image && typeof image === 'string') {
+            formData.append('fileUrl', image);
+        }
+        try {
+            if (validateForm('article', formData)) {
+                setloading(true)
+                await articleService.editArticle(id, formData)
+                navigate(Path.Blog)
+                setloading(false)
+            }
+        } catch (error) {
+            console.log(error.message);
+        }
+    });
+
+    useEffect(() => {
+        articleService.getById(id).then(result => {
+            setArticle(result)
+        }).catch(err => console.log(err))
+    }, [id])
+
+    return (
+        <>
+            {isloading ? <Preloader /> :
+                <div className={styles.postContainer}>
+                    <div className={styles.postForm}>
+                        <form onSubmit={onSubmit}>
+                            <h2 className={styles.title}>Редактирай Пост</h2>
+
+                            <div className={styles.formGroup}>
+                                <label htmlFor="title">Име:</label>
+                                <input
+                                    id="title"
+                                    className={styles.formControl}
+                                    name="title"
+                                    type="text"
+                                    placeholder="Enter Title"
+                                    value={data.title}
+                                    onChange={onChange}
+                                />
+                                {errors.title && <span className={styles.error}>{errors.title}</span>}
+                            </div>
+
+                            <div className={styles.formGroup}>
+                                <label htmlFor="description">Описание:</label>
+                                <textarea
+                                    id="description"
+                                    className={styles.formControl}
+                                    name="description"
+                                    cols="30"
+                                    rows="10"
+                                    value={data.description}
+                                    onChange={onChange}
+                                ></textarea>
+                                {errors.description && <span className={styles.error}>{errors.description}</span>}
+                            </div>
+
+                            <div className={styles.formGroup}>
+                                <label htmlFor="file">
+                                    <img
+                                        className={styles.pic}
+                                        src={image ? image : upload_pic}
+                                        alt=""
+                                    />
+                                </label>
+                                <input
+                                    id="file"
+                                    type="file"
+                                    className={styles.formControl}
+                                    name="file"
+                                    placeholder="Select File"
+                                    onChange={onImageChange} />
+                                {errors.file && <span className={styles.error}>{errors.file}</span>}
+                            </div>
+                            <button type="submit" className={styles.editBtn}>Редактирай</button>
+                        </form>
+                    </div>
+                </div>
+            }
+        </>
+    )
+}
