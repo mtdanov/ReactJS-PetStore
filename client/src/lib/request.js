7c940e74582976e1b90a24cfa48dc4c7dfdcafd0
mtdanov <mtdanov@gmail.com>
2024-08-15 16:30:39 +0300

    add requester

---
+++ client/src/lib/request.js
@@ -0,0 +1,51 @@
+import { getAccessToken } from "../utils/authUtils";
+
+const buildOptions = (data) => {
+    const options = {};
+    if (data instanceof FormData) {
+        options.body = data;
+    } else if(data) {
+        options.body = JSON.stringify(data);
+        options.headers = {
+            'Content-Type': 'application/json'
+        };
+    }
+
+   const token = getAccessToken();
+
+  
+    if (token) {
+        
+        options.headers = {
+            ...options.headers,
+            'X-Authorization': token
+        }; 
+    }
+
+    return options;
+};
+
+const request = async (method, url, data) => {
+    
+    const response = await fetch(url, {
+        ...buildOptions(data),
+        method,
+    });
+
+    if (response.status === 204) {
+        return {};
+    }
+
+    const result = await response.json();
+
+    if (!response.ok) {
+        throw result;
+    } 
+
+    return result;
+};
+
+export const get = request.bind(null, 'GET');
+export const post = request.bind(null, 'POST');
+export const put = request.bind(null, 'PUT');
+export const remove = request.bind(null, 'DELETE');
